7cd1ce9e3cec2253e81ecd8c827ee8ba961d7999
Sara-Waleed <sarawaleedmohamed2@gmail.com>
2024-04-09 22:47:32 +0200

    adding model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:todolist/Presentation/View/Screens/HomePage.dart';
-
 import 'Presentation/View/Screens/Splash.dart';
+
 
 void main() {
   runApp(
